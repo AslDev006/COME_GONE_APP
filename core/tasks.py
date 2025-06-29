d9a866c581ae1b25eee6069acbc645d6c68a5d20
AslDev006 <asldev006@gmail.com>
2025-06-29 11:58:32 +0500

    lunch task

--- core/tasks.py
+++ core/tasks.py
@@ -25,7 +25,7 @@ def run_auto_checkout_celery():
         try:
             with transaction.atomic():
                 local_now = timezone.localtime(task_start_time)
-                checkout_time = local_now.replace(hour=12, minute=0, second=0, microsecond=0)
+                checkout_time = local_now.replace(hour=15, minute=0, second=0, microsecond=0)
 
                 checkout_instance = ComeGoneTimeModel.objects.create(
                     user=user,
@@ -42,4 +42,38 @@ def run_auto_checkout_celery():
         except Exception as e:
             logger.error(f"ERROR: Failed to auto-checkout user '{user.full_name}': {e}", exc_info=True)
 
-    return f"Task finished successfully. Processed {processed_count} users."
+    return f"Checkout task finished successfully. Processed {processed_count} users."
+
+
+@shared_task
+def get_out_lunch_celery():
+    task_start_time = timezone.now()
+    logger.info(f"CELERY TASK STARTED: Auto checkout at [{task_start_time:%Y-%m-%d %H:%M:%S}]")
+
+    users_to_lunch = UserModel.objects.filter(status=True)
+    if not users_to_lunch.exists():
+        logger.info("No users found to auto-checkout. Task finished.")
+        return "No users found to auto-checkout."
+    processed_count = 0
+    for user in users_to_lunch:
+        try:
+            with transaction.atomic():
+                local_now = timezone.localtime(task_start_time)
+                checkout_time = local_now.replace(hour=12, minute=0, second=0, microsecond=0)
+
+                checkout_instance = ComeGoneTimeModel.objects.create(
+                    user=user,
+                    status=False,
+                    time=checkout_time
+                )
+                user.status = False
+                user.save()
+
+            send_auto_checkout_message(checkout_instance)
+            logger.info(f"SUCCESS: Lunch get out out user '{user.full_name}'.")
+            processed_count += 1
+
+        except Exception as e:
+            logger.error(f"ERROR: Failed to auto-checkout user '{user.full_name}': {e}", exc_info=True)
+
+    return f"Lunch task finished successfully. Processed {processed_count} users."
